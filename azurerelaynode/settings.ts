6b5edbf55aa6ce08547518bb19d937c37a193a80
Rodrigo Cabello <rodrigo.cabello.malagon@outlook.es>
2018-01-10 08:16:35 +0100

    Initial commit

---
+++ azurerelaynode/settings.ts
@@ -0,0 +1,8 @@
+export class AppSettings {
+    public static readonly NAMESPACE = '';
+    public static readonly PATH = '';
+    public static readonly KEYRULE = '';
+    public static readonly KEY = '';
+    public static readonly VIEWSFOLDER = './views';
+    public static readonly STATICFOLDERS = './public';
+}
